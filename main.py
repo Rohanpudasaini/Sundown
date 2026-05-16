5558632b69d3a6df4d99c6abc237820157dea0cb
Rohan <pudasainirohan0@gmail.com>
2026-05-16 15:02:58 +0545

    feat: connection entries router in main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from users.router import app as user_router
+from entries.router import app as entry_router
 
 app = FastAPI(title="Sundown", version="0.0.1")
 
 app.include_router(user_router)
+app.include_router(entry_router)
 
 
 @app.get("/")
